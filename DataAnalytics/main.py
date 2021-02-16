9e62d814886cd3ee8565523f8f95004c987d0e2c
renacin <renacin.matadeen@ryerson.ca>
2021-02-15 19:49:33 -0500

    Remove Redundant Rows; Dependent Of Time Remaining & Common Data

--- DataAnalytics/main.py
+++ DataAnalytics/main.py
@@ -4,12 +4,14 @@
 #
 # ----------------------------------------------------------------------------------------------------------------------
 import sqlite3
+import datetime
 import pandas as pd
 # ----------------------------------------------------------------------------------------------------------------------
 
 class prep_data:
 
-    # Read Data From SQLite Database As Pandas Df
+
+    # [FUNCTION #1] Read Data From SQLite Database As Pandas Df
     @staticmethod
     def data_from_db(db_path):
 
@@ -18,12 +20,41 @@ class prep_data:
 
         return pd.read_sql_query(query, conn)
 
-    # Clean Datetime Columns & Create A Date Obsserved Column
-    @staticmethod
-    def clean_dates(df):
 
-        df['START_DATE'] = pd.to_datetime(df['START_DATE'], format='%m/%d/%Y')
-        df['END_DATE'] = pd.to_datetime(df['END_DATE'], format='%m/%d/%Y')
+    # Function For  [FUNCTION #2] Parse Item Type
+    def item_type(sku):
+        if sku[-1] == "F":
+            return "Jewelry"
+
+        elif sku[-1] == "B":
+            return "Technology"
+
+        elif sku[-1] == "C":
+            return "Antique"
+
+        return "N/A"
+
+
+    # [FUNCTION #2] Clean Datetime Columns & Create A Date Obsserved Column
+    @staticmethod
+    def clean_columns(df):
+
+        df["START_DATE"] = pd.to_datetime(df["START_DATE"], format="%m/%d/%Y")
+        df["END_DATE"] = pd.to_datetime(df["END_DATE"], format="%m/%d/%Y")
+        df["LIST_DAY"] = df["START_DATE"].dt.day_name()
+        df["LIST_MONTH"] = df["START_DATE"].dt.month_name()
+        df["ITEM_TYPE"] = [prep_data.item_type(x) for x in df["SKU"]]
+
+        # Drop Items Where Type Is N/A & Num Bids Is Zero
+        df = df[df["ITEM_TYPE"] != "N/A"]
+        df = df[df["NUM_BIDS"] != 0]
+
+        return df
+
+
+    # [FUNCTION #3] Remove Redundant Rows
+    @staticmethod
+    def remove_redun(df):
 
         return df
 
@@ -32,9 +63,11 @@ class prep_data:
 # Main Function That Will Store Everything
 def main():
     df = prep_data.data_from_db("TPA_Items_DB.db")
-    df = prep_data.clean_dates(df)
+    df = prep_data.clean_columns(df)
+    df = prep_data.remove_redun(df)
 
-    print(df.head(100))
+    df.to_csv("DEBUG.csv", index=False)
+
 
 # ----------------------------------------------------------------------------------------------------------------------
 
